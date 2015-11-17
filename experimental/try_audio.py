2e41a866935bce45e5fb6fe4a8409ce0eb93863d
Tim Levine <timtiptoes@yahoo.com>
2015-11-16 16:03:11 -0800

    change timing

--- experimental/try_audio.py
+++ experimental/try_audio.py
@@ -62,16 +62,17 @@ class simpleapp_tk():
         'Z': '--..',
         '_': '..--.-'}
 
-    factor=1
+    factor=0.1
 
 
     #What to do when user presses OK
     def __init__(self):
-        input="e"
+        subprocess.call(["amixer","-D", "pulse", "sset", "Master", "4%"])
+        input=raw_input("enter message:")
           #    1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
-        self.factor=100
-        (self.factor,self.audio_dot,self.audio_dash) = get_speed_factors(input)
-
+        self.factor=0.1
+        (self.factor,self.audio_dot,self.audio_dash) = self.get_speed_factors(input)
+	print "using these speed factors:" +str(self.factor)+","+self.audio_dot+","+self.audio_dash
         coded_output=""
         for letter in input:
             print letter*20
@@ -100,7 +101,7 @@ class simpleapp_tk():
     def dot(self):
         print "dot"
         print "open shutter"
-        subprocess.call(["cvlc", "--play-and-exit", "/home/tim/github/stargazing/data/dot.wav"])
+   #     subprocess.call(["cvlc", "--play-and-exit", "/home/tim/github/stargazing/data/"+self.audio_dot])
         time.sleep(self.factor)
         print "close shutter"
         time.sleep(self.factor)
@@ -108,14 +109,12 @@ class simpleapp_tk():
     def dash(self):
         print "dash"
         print "shutter open"
-        subprocess.call(["amixer","-D", "pulse", "sset", "Master", "4%"])
-
-        subprocess.call(["cvlc", "--play-and-exit", "/home/tim/github/stargazing/data/dash.wav"])
+        #subprocess.call(["cvlc", "--play-and-exit", "/home/tim/github/stargazing/data/"+self.audio_dash])
         time.sleep(3*self.factor)
         print "shutter closed"
         time.sleep(self.factor)
 
-    def get_speed_factors(input):
+    def get_speed_factors(self,input):
         if (len(input)<=10):
             return (1,'audiocheck.net_sin_800Hz_-3dBFS_1.0s.wav','audiocheck.net_sin_800Hz_-3dBFS_3s.wav')
         elif (len(input)<=50):
